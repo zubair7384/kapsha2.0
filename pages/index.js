f8b56076e689456f6fc373f9a2e82c8fee7d74dc
zubair7384 <zubair.iqbal2018@gmail.com>
2022-04-21 11:57:42 +0400

    added mata des and title

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styles from "../styles/Home.module.css";
+import Head from "next/head";
 import Header from "../common/components/Header.jsx";
 import Main from "../common/components/Main.jsx";
 import Footer from "../common/components/Footer.jsx";
@@ -11,6 +12,13 @@ export default function Home() {
   };
   return (
     <div className={!dark ? styles.AppWrapper : styles.AppDarkWrapper}>
+      <Head>
+        <title>kapsha - blockchain powered disruptive fashion</title>
+        <meta
+          name="description"
+          content="Kapsha is a blockchain fashion startup. Redefine the legacy of luxury fashion, own and feel NFTs in the real world. Launching soon."
+        />
+      </Head>
       <Header handleDark={handleDark} night={dark} />
       <Main night={dark} />
       <Footer night={dark} />
